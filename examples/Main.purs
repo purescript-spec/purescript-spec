a1b7ce8be8bc7b490333b3abbcb836bd137ed861
Oskar Wickström <oskar.wickstrom@live.com>
2015-04-21 12:15:07 +0200

    Add QuickCheck example

--- examples/Main.purs
+++ examples/Main.purs
@@ -1,8 +1,9 @@
 module Main where
 
-import Test.Spec
+import Test.Spec (describe, pending, it)
 import Test.Spec.Node
 import Test.Spec.Assertions
+import Test.QuickCheck
 
 additionSpec =
   describe "Addition" do
@@ -11,8 +12,12 @@ additionSpec =
     it "fails as well" do
       (1 + 1) `shouldEqual` 3
 
-main = runNode $
+main = runNode do
   describe "Math" do
     additionSpec
     describe "Multiplication" do
       pending "will do multiplication in the future"
+  describe "Tools" $
+    describe "QuickCheck" $
+      it "works too" $
+        quickCheck \n -> n + 1 > n
