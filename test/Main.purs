34f101eda2c9943b554777909a938ec07f1f18a0
Josh Bassett <josh.bassett@gmail.com>
2016-03-15 09:42:20 +1100

    Adopt the uppercase convention for the process effect name.

--- test/Main.purs
+++ test/Main.purs
@@ -5,14 +5,14 @@ import Prelude
 import Control.Monad.Eff         (Eff())
 import Control.Monad.Eff.Console (CONSOLE())
 
-import Test.Spec.Runner           (Process(), run)
+import Test.Spec.Runner           (PROCESS(), run)
 import Test.Spec.Reporter.Console (consoleReporter)
 
 import Test.Spec.ReporterSpec   (reporterSpec)
 import Test.Spec.RunnerSpec     (runnerSpec)
 import Test.Spec.AssertionSpec  (assertionSpec)
 
-main :: forall eff. Eff (console :: CONSOLE, process :: Process | eff) Unit
+main :: forall eff. Eff (console :: CONSOLE, process :: PROCESS | eff) Unit
 main = run [consoleReporter] do
   runnerSpec
   reporterSpec
