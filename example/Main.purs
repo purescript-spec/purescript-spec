1b20ee4be1a88fc4eabad41bd21ef9c988f81670
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-01-11 19:28:36 +0100

    Update example image

--- example/Main.purs
+++ example/Main.purs
@@ -2,11 +2,24 @@ module Main where
 
 import Prelude
 import Control.Monad.Aff (later')
+import Control.Monad.Aff.AVar (AVAR)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Timer (TIMER)
+import Node.Process (PROCESS)
 import Test.Spec (pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
 
+main
+  :: Eff
+     ( process :: PROCESS
+     , console :: CONSOLE
+     , timer :: TIMER
+     , avar :: AVAR
+     )
+     Unit
 main = run [consoleReporter] do
   describe "purescript-spec" do
     describe "Attributes" do
@@ -17,7 +30,8 @@ main = run [consoleReporter] do
     describe "Features" do
       it "runs in NodeJS" $ pure unit
       it "runs in the browser" $ pure unit
+      it "supports streaming reporters" $ pure unit
       it "supports async specs" do
         res <- later' 100 $ pure "Alligator"
         res `shouldEqual` "Alligator"
-      it "is PureScript 0.10.1 compatible" $ pure unit
+      it "is PureScript 0.10.x compatible" $ pure unit
