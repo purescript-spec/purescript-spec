465478ecd81af808e8f91f3690e06d42e32e1034
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-02-01 20:29:23 +0100

    Remove unused Reporter module

--- test/Main.purs
+++ test/Main.purs
@@ -4,12 +4,10 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Test.Spec.AssertionSpec (assertionSpec)
 import Test.Spec.Reporter.Dot (dotReporter)
-import Test.Spec.ReporterSpec (reporterSpec)
 import Test.Spec.Runner (RunnerEffects, run)
 import Test.Spec.RunnerSpec (runnerSpec)
 
 main :: Eff (RunnerEffects ()) Unit
 main = run (dotReporter { width: 80, slow: 800 }) do
   runnerSpec
-  reporterSpec
   assertionSpec
