d68518450c23e871adb4a95fea5aeb91958a993e
Oskar Wickström <oskar.wickstrom@gmail.com>
2017-02-08 18:14:19 +0100

    Reexport specific reporters under Test.Spec.Reporter

--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,7 @@ module Test.Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Test.Spec.AssertionSpec (assertionSpec)
-import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (RunnerEffects, run)
 import Test.Spec.RunnerSpec (runnerSpec)
 
