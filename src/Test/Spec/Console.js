edffb63118aca6b2ba55b16001ecad2c5b328898
joneshf <jones3.hardy@gmail.com>
2017-09-26 04:28:28 -0700

    Use `-ansi` instead of FFI

--- src/Test/Spec/Console.js
+++ src/Test/Spec/Console.js
@@ -24,8 +24,3 @@ exports.write = function(s) {
     }
   };
 };
-
-// This needs a foreign function to support the escape sequence.
-exports._setAttr = function (codeStr) {
-  return exports.write("\x1b[" + codeStr + "m");
-};
