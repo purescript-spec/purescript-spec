75018f3629ed82fa40d66722eed87dfef8e1ff14
Felix Mulder <felix.mulder@gmail.com>
2018-12-04 08:45:25 +0100

    Redefine `run` and `run'` to return `Aff Unit`

--- test/Main.purs
+++ test/Main.purs
@@ -2,12 +2,13 @@ module Test.Main where
 
 import Prelude
 import Effect (Effect)
+import Effect.Aff (launchAff_)
 import Test.Spec.AssertionSpec (assertionSpec)
 import Test.Spec.Reporter (consoleReporter)
 import Test.Spec.Runner (run)
 import Test.Spec.RunnerSpec (runnerSpec)
 
 main :: Effect Unit
-main = run [ consoleReporter ] do
+main = launchAff_ $ run [ consoleReporter ] do
   runnerSpec
   assertionSpec
