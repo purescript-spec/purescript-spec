7e0885a2dbbffd7e2a3a26f19f0a8a7597bdbfad
neppord <neppord@gmail.com>
2023-01-07 20:23:56 +0100

    Use nodeId and parentNodeId in teamcity reporter
    This is so that the tests and suites don't need to be executed in order or within their suite

--- src/Test/Spec/Reporter/TeamCity.purs
+++ src/Test/Spec/Reporter/TeamCity.purs
@@ -8,7 +8,9 @@ import Data.Array ((:))
 import Data.Array (intercalate) as Array
 import Data.Int (trunc)
 import Data.Map.Internal as Map
+import Data.Maybe (Maybe, fromMaybe)
 import Data.Maybe (fromMaybe) as Maybe
+import Data.Newtype (unwrap)
 import Data.String.Regex (replace') as Regex
 import Data.String.Regex.Flags (global) as Regex
 import Data.String.Regex.Unsafe (unsafeRegex) as Regex
@@ -20,6 +22,7 @@ import Test.Spec.Reporter.Base (defaultReporter)
 import Test.Spec.Result (Result(..))
 import Test.Spec.Runner (Reporter)
 import Test.Spec.Runner.Event (Event(..)) as Event
+import Test.Spec.Tree (Path, parentSuite)
 
 escape :: String -> String
 escape = Regex.replace'
@@ -39,51 +42,92 @@ teamcity name properties = "##teamcity[" <> body <> "]"
   body = name : (properties <#> renderProperty) # Array.intercalate " "
   renderProperty (key /\ value) = key <> "='" <> escape value <> "'"
 
+message :: forall a. String -> ServiceMessage a -> String
+message event { name, nodeId, parentNodeId } = teamcity event
+  [ "name" /\ name
+  , "nodeId" /\ nodeId
+  , "parentNodeId" /\ (fromMaybe "0" parentNodeId)
+  ]
+
 testCount :: Int -> String
 testCount count = teamcity "testCount" [ "count" /\ show count ]
 
-testSuiteStarted :: Array (Tuple String String) -> String
-testSuiteStarted = teamcity "testSuiteStarted"
+testSuiteStarted :: ServiceMessage () -> String
+testSuiteStarted = message "testSuiteStarted"
 
-testSuiteFinished :: Array (Tuple String String) -> String
-testSuiteFinished = teamcity "testSuiteFinished"
+testSuiteFinished :: ServiceMessage () -> String
+testSuiteFinished = message "testSuiteFinished"
 
-testStarted :: Array (Tuple String String) -> String
-testStarted = teamcity "testStarted"
+testFinishedIn :: WithDuration -> String
+testFinishedIn { name, nodeId, parentNodeId, duration } =
+  teamcity "testFinished"
+    [ "name" /\ name
+    , "nodeId" /\ nodeId
+    , "duration" /\ show (trunc duration)
+    , "parentNodeId" /\ (fromMaybe "0" parentNodeId)
+    ]
 
-testIgnored :: Array (Tuple String String) -> String
-testIgnored = teamcity "testIgnored"
+testFailed :: WithMessage -> String
+testFailed { name, nodeId, parentNodeId, message } =
+  teamcity "testFailed"
+    [ "name" /\ name
+    , "nodeId" /\ nodeId
+    , "message" /\ message
+    , "parentNodeId" /\ (fromMaybe "0" parentNodeId)
+    ]
 
-testFinished :: Array (Tuple String String) -> String
-testFinished = teamcity "testFinished"
+type ServiceMessage x =
+  { name :: String
+  , nodeId :: String
+  , parentNodeId :: Maybe String
+  | x
+  }
 
-testFailed :: Array (Tuple String String) -> String
-testFailed = teamcity "testFailed"
+serviceMessage :: String -> Path -> ServiceMessage ()
+serviceMessage name' path =
+  let
+    name = name'
+    nodeId = idFromPath path
+    parentNodeId = parentSuite path
+      <#> _.path
+      <#> idFromPath
+  in
+    { name, nodeId, parentNodeId }
+
+type WithMessage = ServiceMessage (message :: String)
+type WithDuration = ServiceMessage (duration :: Number)
+
+idFromPath :: Path -> String
+idFromPath path = path
+  <#> unwrap
+  <#> (\{ index, name } -> show index <> ":" <> fromMaybe "" name)
+  # Array.intercalate ","
 
 teamcityReporter :: Reporter
 teamcityReporter = defaultReporter Map.empty case _ of
   Event.Suite _ path name -> do
     void $ modify $ Map.insert path name
-    tellLn $ testSuiteStarted [ "name" /\ name ]
+    tellLn $ testSuiteStarted $ serviceMessage name path
   Event.SuiteEnd path -> do
     name <- get <#> Map.lookup path <#> Maybe.fromMaybe ""
-    tellLn $ testSuiteFinished [ "name" /\ name ]
-  Event.Test _ _ name ->
-    tellLn $ testStarted [ "name" /\ name ]
-  Event.Pending _ name -> do
-    tellLn $ testStarted [ "name" /\ name ]
-    tellLn $ testIgnored [ "name" /\ name ]
-    tellLn $ testFinished [ "name" /\ name ]
-  Event.TestEnd _ name (Success _ (Milliseconds millies)) -> do
-    tellLn $ testFinished
-      [ "name" /\ name
-      , "duration" /\ show (trunc millies)
-      ]
-  Event.TestEnd _ name (Failure error) -> do
-    tellLn $ testFailed
-      [ "name" /\ name
-      , "message" /\ show error
-      ]
-    tellLn $ testFinished [ "name" /\ name ]
+    tellLn $ testSuiteFinished $ serviceMessage name path
+  Event.Test _ path name -> do
+    tellLn $ message "testStarted" $ serviceMessage name path
+  Event.Pending path name -> do
+    let attributes = serviceMessage name path
+    tellLn $ message "testStarted" attributes
+    tellLn $ message "testIgnored" attributes
+    tellLn $ message "testFinished" attributes
+  Event.TestEnd path name' (Success _ (Milliseconds millies)) -> do
+    let
+      { name, nodeId, parentNodeId } = serviceMessage name' path
+      attributes = { name, nodeId, parentNodeId, duration: millies }
+    tellLn $ testFinishedIn attributes
+  Event.TestEnd path name' (Failure error) -> do
+    let
+      { name, nodeId, parentNodeId } = serviceMessage name' path
+      attributes = { name, nodeId, parentNodeId, message: show error }
+    tellLn $ testFailed attributes
+    tellLn $ message "testFinished" attributes
   Event.End _ -> pure unit
   Event.Start count -> tellLn $ testCount count
